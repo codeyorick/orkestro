577161ee699202b2384e7fda3716c4d7623e8808
Niels Krijnen <niels.krijnen@icloud.com>
2025-05-14 14:09:16 +0200

    Add Button component and reorganize assets directory
    Introduced a new `Button` component with Svelte implementation and its corresponding Storybook story. Moved all asset files from `src/stories/assets` to `.storybook/assets` for better organization and consistency in the project structure.

---
+++ src/lib/components/ui/Button.stories.svelte
@@ -0,0 +1,11 @@
+<script module>
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import Button from "$lib/components/ui/Button.svelte";
+
+  const { Story } = defineMeta({
+    title: "UI/Button",
+    component: Button
+  })
+</script>
+
+<Story name="Default"/>
---
+++ src/lib/components/ui/Button.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import type { HTMLButtonAttributes } from "svelte/elements";
+
+  let { children, ...rest }: HTMLButtonAttributes = $props()
+</script>
+
+<button aria-label="button" {...rest} class={["btn", rest.class]}>
+  {@render children?.()}
+</button>
