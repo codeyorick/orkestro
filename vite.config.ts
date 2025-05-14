13489041099ff2392474608bcf8e0135f508b087
Yorick Nap <yorick@napmail.nl>
2025-05-14 14:38:10 +0200

    Added docker-compose.yml for development

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		allowedHosts: [ process.env.VITE_ALLOWED_HOSTS || 'localhost' ],
+	}
 });
