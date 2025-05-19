876e2e87969ca5bdd351e2903985813d514067ba
Yorick Nap <yorick@napmail.nl>
2025-05-19 15:44:00 +0200

    Add Header and Footer components to layout

---
+++ src/lib/components/layout/Footer.stories.svelte
@@ -0,0 +1,12 @@
+<script module lang="ts">
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Footer from '$lib/components/layout/Footer.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Layout/Footer',
+		component: Footer,
+		tags: ['autodocs']
+	});
+</script>
+
+<Story name="Default" />
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 	import '../app.css';
+	import { Header, Footer } from '$lib/components/layout';
 
 	let { children } = $props();
 </script>
 
-<main class="p-4">
-  {@render children()}
-</main>
+<div class="flex flex-col min-h-screen">
+	<Header />
+	<main class="p-4 flex-grow">
+		{@render children()}
+	</main>
+	<Footer />
+</div>
