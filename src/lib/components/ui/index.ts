4404d5996518bdb370a348edea8b0fba244b47ef
Yorick Nap <yorick@napmail.nl>
2025-05-19 14:37:39 +0200

    Add FileInput component for drag-and-drop file uploads

--- src/lib/components/ui/index.ts
+++ src/lib/components/ui/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from './Button.svelte';
 export { default as Fieldset } from './Fieldset.svelte';
+export { default as FileInput } from './FileInput.svelte';
 export { default as Input } from './Input.svelte';
 export { default as Link } from './Link.svelte';
