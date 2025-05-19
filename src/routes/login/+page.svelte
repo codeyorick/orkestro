ef1fa88b2640d5eb4653f19f27a34977ef82e85a
Yorick Nap <yorick@napmail.nl>
2025-05-19 17:10:45 +0200

    Add OAuth login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,10 +1,28 @@
 <script lang="ts">
-  import { enhance } from "$app/forms";
-  import { Button } from "$lib/components/ui";
+	import { enhance } from '$app/forms';
+	import { Button, Fieldset, Input } from '$lib/components/ui';
+	import { PUBLIC_APPWRITE_ENDPOINT, PUBLIC_APPWRITE_PROJECT } from '$env/static/public';
+
+	import { Client, Account, OAuthProvider } from 'appwrite';
+
+	const client = new Client()
+		.setEndpoint(PUBLIC_APPWRITE_ENDPOINT)
+		.setProject(PUBLIC_APPWRITE_PROJECT);
+
+	function oauth(provider: OAuthProvider) {
+		const account = new Account(client);
+		account.createOAuth2Session(provider);
+	}
 </script>
 
-<form method="POST" use:enhance class="flex flex-col items-center gap-4">
-  <input type="email" name="email" class="input" placeholder="Email">
-  <input type="password" name="password" class="input" placeholder="Wachtwoord">
-  <Button type="submit">Login</Button>
-</form>
+<div class="flex flex-col items-stretch gap-4 max-w-xs mx-auto">
+	<form method="POST" use:enhance class="flex flex-col items-stretch gap-2">
+		<Fieldset legend="Inloggen">
+			<Input type="email" name="email" label="E-mailadres" />
+			<Input type="password" name="password" label="Wachtwoord" />
+		</Fieldset>
+		<Button type="submit">Login</Button>
+	</form>
+	<div class="divider">of</div>
+	<Button onclick={() => oauth(OAuthProvider.Github)}>Login met GitHub</Button>
+</div>
