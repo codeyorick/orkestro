c017faf51e0fa619f0cd52f6ec2fced3ad03bc0d
Yorick Nap <yorick@napmail.nl>
2025-08-18 14:26:24 +0200

    Remove unnecessary `throw` statements in OAuth route redirections

--- src/routes/auth/oauth/+server.ts
+++ src/routes/auth/oauth/+server.ts
@@ -3,19 +3,19 @@ import { redirect, type RequestHandler } from '@sveltejs/kit';
 export const GET: RequestHandler = async ({ url, locals }) => {
 	if (url.searchParams.get('error')) {
 		const searchParams = new URLSearchParams(url.searchParams);
-		throw redirect(303, `/auth/error?${searchParams.toString()}`);
+		redirect(303, `/auth/error?${searchParams.toString()}`);
 	}
 
 	const code = url.searchParams.get('code');
 
 	if (!code) {
-		throw redirect(303, `/auth/error?error=no-code`);
+		 redirect(303, `/auth/error?error=no-code`);
 	}
 
 	const { error } = await locals.supabase.auth.exchangeCodeForSession(code);
 	if (error) {
-		throw redirect(303, `/auth/error?error=exchange-failed`);
+		redirect(303, `/auth/error?error=exchange-failed`);
 	}
 
-	throw redirect(303, `/`);
+	redirect(303, `/`);
 };
